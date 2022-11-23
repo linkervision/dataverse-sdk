24d7918c1bb465a7d064ad49c08b0c25f58cd266
yihsuan123 <myemail@example.com>
2022-11-23 12:06:45 +0800

    feat(python): update for pypi package upload

--- python/setup.py
+++ python/setup.py
@@ -1,19 +1,22 @@
-import os.path as osp
-
 from setuptools import find_packages, setup
 
-PACKAGE_NAME = "dataverse_sdk"
+AUTHOR = "LinkerVision"
+PACKAGE_NAME = "dataverse-sdk"
 PACKAGE_VERSION = "0.1.0"
-ROOT = osp.abspath(osp.dirname(__file__))
-
-with open(osp.join(ROOT, "requirements/requirements.txt")) as f:
-    requires = f.read().split("\n")
+DESC = "Dataverse SDK For Python"
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
     packages=find_packages(),
     python_requires=">=3.9, <4",
-    author="LinkerVision",
-    install_requires=[requires],
+    author=AUTHOR,
+    url="",
+    description=DESC,
+    install_requires=["pydantic", "requests"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python :: 3"],
 )
