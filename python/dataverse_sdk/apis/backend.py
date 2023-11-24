0328ca26234c455603ef45b291a9da16409dc655
christianmasa <christian@linkervision.com>
2023-11-24 17:58:11 +0800

    fix(python): update access key validation logic

--- python/dataverse_sdk/apis/backend.py
+++ python/dataverse_sdk/apis/backend.py
@@ -280,10 +280,9 @@ class BackendAPI:
         }
 
         aws_access_key = {secret_access_key, access_key_id}
-
-        if not all(not access_key for access_key in aws_access_key):
+        if not (all(aws_access_key) or not any(aws_access_key)):
             raise ValueError("Need to assign both secret_access_key and access_key_id")
-        elif all(access_key for access_key in aws_access_key):
+        elif all(aws_access_key):
             payload_data.update(
                 {"secret_access_key": secret_access_key, "access_key_id": access_key_id}
             )
